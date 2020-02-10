e20c63409b40a139a7cd4c6b38aa100ef6f8fc7f
estelamaga <estelinha.mag@hotmail.com>
2020-02-10 18:26:56 -0300

    Controle Dinâmico

---
+++ spec/dynamic_control_spec.rb
@@ -0,0 +1,23 @@
+describe "controle dinâmico", :dc do
+  before(:each) do
+    visit "https://training-wheels-protocol.herokuapp.com/dynamic_controls"
+  end
+
+  #Cenário
+  # Quando tem a propriedade disabled true o campo está desabilitado.
+  #Quando tem a propiedade disabled o campo está habilitado.
+
+  it "Quando habilita o campo" do
+    res = page.has_field? "movie", disabled: true
+    puts res
+
+    click_button "Habilita"
+
+    res = page.has_field? "movie", disabled: false
+    puts res
+  end
+end
+
+#Observaç?es
+#Perguntando se existe um identificador com o campo movie
+# O ponto de ? traz um valor TRUE ou FALSE
