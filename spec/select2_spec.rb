b19de902d3181637b123178cec51e0c92045b893
estelamaga <estelinha.mag@hotmail.com>
2020-02-12 23:19:36 -0300

    Automatizando opção com select2

--- spec/select2_spec.rb
+++ spec/select2_spec.rb
@@ -1,9 +1,16 @@
 describe "Select 2", :select2 do
-  describe "single"do
+  describe "single", :single do
     before(:each)do
-      visit "/apps/select2/single.html"
+      visit "https://training-wheels-protocol.herokuapp.com/apps/select2/single.html"
     end
-  
+
+    it "Selecionar ator por nome"do
+      find(".select2-selection--single").click
+      sleep 1
+      find(".select2-results__option", text: 'Adam Sandler').click
+      sleep 5
+    end
+  end
     
     describe "multiple"do
     before(:each)do
