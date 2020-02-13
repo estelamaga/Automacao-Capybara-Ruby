ca9fbcdd3f515f0b0cf72783f5e546944d2944bd
estelamaga <estelinha.mag@hotmail.com>
2020-02-12 22:51:50 -0300

    Automatizando opção com select2

---
+++ spec/select2_spec.rb
@@ -0,0 +1,13 @@
+describe "Select 2", :select2 do
+  describe "single"do
+    before(:each)do
+      visit "/apps/select2/single.html"
+    end
+  
+    
+    describe "multiple"do
+    before(:each)do
+      visit "/apps/select2/single.html"
+    end
+  end
+end
